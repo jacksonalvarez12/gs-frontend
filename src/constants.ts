935f33b1201c98fb019adee5e122148feb4fb7eb
Jackson <jacksonalvarezacc@gmail.com>
2024-07-16 21:30:30 -0400

    http -> https

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const paths = {
 
 export const constants = {
     spotifyClientId: '520783e7b2cb40fc870b91e3761d960f',
-    redirectUrl: 'http://spotifygroups.com/',
+    redirectUrl: 'https://spotifygroups.com/',
 };
